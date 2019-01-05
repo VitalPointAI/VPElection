b1b2a8ed7ecb9279d166149fd70008b56e28b2d9
VitalPointAI <a.luhning@vitalpoint.ai>
2019-01-05 12:36:29 -0500

    first commit

---
+++ contracts/Election.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.5.0;
+
+contract Election {
+    //Store Candidate
+
+    //Read Candidate
+    string public candidate;
+
+    //Constructor
+    constructor() public {
+        candidate = "Candidate 1";
+    }
+}
