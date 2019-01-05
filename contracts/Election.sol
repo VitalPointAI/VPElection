ec5126e21ab70bcbf360e27c8f0fb0d1c2937a62
VitalPointAI <a.luhning@vitalpoint.ai>
2019-01-05 15:03:25 -0500

    second commit

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,13 +1,28 @@
 pragma solidity ^0.5.0;
 
 contract Election {
-    //Store Candidate
+    // Model a Candidate
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
 
-    //Read Candidate
-    string public candidate;
+    // Store Candidates
+    // Fetch Candidate
+    mapping(uint => Candidate) public candidates;
+
+    // Store Candidates Count
+    uint public candidatesCount;
 
     //Constructor
     constructor() public {
-        candidate = "Candidate 1";
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
+    }
+
+    function addCandidate (string memory _name) private {
+        candidatesCount++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
     }
 }
